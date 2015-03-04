1fa9ace7050b29646601a54f98b43d21b8c3b302
Fábio Rocha <fabiorochafg@gmail.com>
2015-03-04 10:15:29 -0300

    Melhorando o layout

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # amanda
 Our child is coming to us!
+
+
+Colors sequence:
+
+#FFEE58 rgba(255, 238, 88, 0.75)
+#FFEB3B rgba(255, 235, 59, 0.75)
+#FDD835 rgba(253, 216, 53, 0.75)
+#FBC02D rgba(251, 192, 45, 0.75)
+#F57F17 rgba(245, 127, 23, 0.75)
+#FF6F00 rgba(255, 111, 0, 0.75)
+#E65100 rgba(230, 81, 0, 0.75)
--- files/main.js
+++ files/main.js
@@ -5,6 +5,20 @@ new WOW().init();
 $(document).ready(function() {
 
 	$("section.equal-height, section.equal-height > div").css("height", $(window).height());
-	$("section[data-order='1'], section[data-order='1'] > div").css("height", $(window).height());
+
+
+	$(window).scroll(function() {
+	    var x = $(this).scrollTop();
+	    // $('section[data-order="5"] > header').css('background-position', 'right ' + (x * .1) + 'px');
+	    // $('section[data-order="5"] > header').css({'background-position': "left " + (x*.1) + "px, " + "right " + (x*.1)+"px"});
+	});
 
 });
+
+/* Google Analyctis: SolutiBrasil */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-41737928-11', 'auto');
+ga('send', 'pageview');
